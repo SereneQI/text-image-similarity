5fd28411926fa3711943cb4a7185093ea746a8db
Maxime <maxime.portaz@gmail.com>
2019-01-30 13:47:25 +0000

    add embeddingFile

--- misc/embedding.py
+++ misc/embedding.py
@@ -4,7 +4,8 @@
 
 from nltk.tokenize import word_tokenize
 import argparse
-
+import fastText
+from utils import encode_sentence_fasttext
 
 def file_len(fname):
     with open(fname) as f:
@@ -31,9 +32,18 @@ def tokenizeFile(filename, file_out):
                 
         
 
-def embedFile(filename, file_out):
-    print("Not implemented yet")
-
+def embedFile(args):
+    print("Compute embeddings for the file:", args.file)
+    embed = fastText.load_model(args.dict)
+    nl = file_len(args.file)
+    fout = open(args.output, 'w')
+    with open(args.file) as f:
+        for i, line in enumerate(f):
+            line = line.rstrip()
+            print("%2.2f" % (i/nl*100.0), "\%", end='\r')
+            i, t = line.split("\t")
+            es = encode_sentence_fasttext(t.split(' '), embed, tokenize=False)
+            fout.write(i+'\t'+str(es)+'\n')
 
 
 if __name__ == '__main__':
@@ -46,7 +56,7 @@ if __name__ == '__main__':
        
     args = parser.parse_args()
     if args.tokenize_only == 'False':
-        embedFile(args.file, args.output)
+        embedFile(args)
     else:
         tokenizeFile(args.file, args.output)
     
